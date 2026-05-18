c68a38713398ce65f0874ae20349c15ee205e68f
dilshod1d <shoolimxonovd@gmail.com>
2026-05-18 11:41:39 +0500

    changed video urls

--- src/features/landing/assets/video-assets.ts
+++ src/features/landing/assets/video-assets.ts
@@ -6,32 +6,32 @@ export interface LandingVideoAsset {
 export const landingVideoAssets = {
   hero: {
     desktop:
-      "https://res.cloudinary.com/dcderdzpp/video/upload/v1778653602/video1v2_df9r9a.mp4",
+      "https://res.cloudinary.com/dhdkedt1m/video/upload/v1779086237/video1v2_sbglcy.mp4",
     mobile:
-      "https://res.cloudinary.com/dcderdzpp/video/upload/v1778843661/video1-mobile_goha9m.mp4",
+      "https://res.cloudinary.com/dhdkedt1m/video/upload/v1779086237/video2-mobile_o8pqut.mp4",
   },
   whyWemWorks: {
     desktop:
-      "https://res.cloudinary.com/dcderdzpp/video/upload/v1778653613/video2v2_gi85w7.mp4",
+      "https://res.cloudinary.com/dhdkedt1m/video/upload/v1779086238/video2v2_m6wjhr.mp4",
     mobile:
-      "https://res.cloudinary.com/dcderdzpp/video/upload/v1778843658/video2-mobile_fynlwn.mp4",
+      "https://res.cloudinary.com/dhdkedt1m/video/upload/v1779086237/video2-mobile_o8pqut.mp4",
   },
   systemFlow: {
     desktop:
-      "https://res.cloudinary.com/dcderdzpp/video/upload/v1778653612/video3v2_hmj8er.mp4",
+      "https://res.cloudinary.com/dhdkedt1m/video/upload/v1779086239/video3v2_rn119q.mp4",
     mobile:
-      "https://res.cloudinary.com/dcderdzpp/video/upload/v1778843658/video3-mobile_m1h6sk.mp4",
+      "https://res.cloudinary.com/dhdkedt1m/video/upload/v1779086239/video3-mobile_yr8gns.mp4",
   },
   howItWorks: {
     desktop:
-      "https://res.cloudinary.com/dcderdzpp/video/upload/v1778653607/video4v2_qtbu47.mp4",
+      "https://res.cloudinary.com/dhdkedt1m/video/upload/v1779086239/video4v2_pnkubg.mp4",
     mobile:
-      "https://res.cloudinary.com/dcderdzpp/video/upload/v1778843658/video4-mobile_nn4twb.mp4",
+      "https://res.cloudinary.com/dhdkedt1m/video/upload/v1779086239/video4-mobile_bdnihw.mp4",
   },
   portfolioResults: {
     desktop:
-      "https://res.cloudinary.com/dcderdzpp/video/upload/v1778653606/video5v2_nybafe.mp4",
+      "https://res.cloudinary.com/dhdkedt1m/video/upload/v1779086238/video5v2_kjyyu2.mp4",
     mobile:
-      "https://res.cloudinary.com/dcderdzpp/video/upload/v1778843658/video5-mobile_jb17wy.mp4",
+      "https://res.cloudinary.com/dhdkedt1m/video/upload/v1779086237/video5-mobile_tslgm5.mp4",
   },
 } satisfies Record<string, LandingVideoAsset>;
